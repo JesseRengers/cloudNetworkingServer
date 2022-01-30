1482bd795902261a9e4242e760d4017507e5f338
Jesse Rengers <j.j.rengers@student.utwente.nl>
2022-01-30 18:15:10 +0100

    gone to a udp echo server

--- udpServer.py
+++ udpServer.py
@@ -10,8 +10,8 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = '127.0.0.1'
-server_port = 31337
+server_address = '0.0.0.0'
+server_port = 33333
 
 server = (server_address, server_port)
 sock.bind(server)
@@ -19,5 +19,8 @@ print("Listening on " + server_address + ":" + str(server_port))
 
 while True:
 	payload, client_address = sock.recvfrom(1024)
-	print("Echoing data back to " + str(client_address) + " : " + payload.decode())
-	sent = sock.sendto(payload, client_address)
+	if payload:
+		response = "Client address: %s:%s\nData sent by client:\n%s\n" % (
+			client_address[0], client_address[1], payload)
+		print(response)
+		sent = sock.sendto(payload, client_address)
